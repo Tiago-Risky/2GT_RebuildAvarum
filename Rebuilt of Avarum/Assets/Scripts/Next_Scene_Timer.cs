772a1c63a22e59cc5e8e1ef94e3193b13a969d95
Tiago Cabral <riskybomberpt@gmail.com>
2020-02-01 00:38:58 +0000

    [INIT]

---
+++ Rebuilt of Avarum/Assets/Scripts/Next_Scene_Timer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Next_Scene_Timer : MonoBehaviour {
+    
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine(Wait());
+	}
+
+    IEnumerator Wait() {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
